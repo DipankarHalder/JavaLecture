f1816c594b995202b0637a86ac02d8311633e1c9
Dipankar Halder <dipankarhalder@Dipankars-MacBook-Pro.local>
2022-05-12 23:44:28 +0530

    first commit

---
+++ FirstClass.java
@@ -0,0 +1,12 @@
+// JRE => Java Runtime Environment
+// JVM => Java Virtual Machine
+// JDK => Java Development Kit
+// Java is Portable language, because you can convert it to byte code and run it in any system.
+
+class FirstClass {
+
+  
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+  }
+}
